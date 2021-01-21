c01ab11ccbddf18d0abc4e1ff9f25e8354d30338
daviidy <yaodavidarmel@gmail.com>
2021-01-21 09:04:51 +0000

    add page load function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import 'bootstrap'
-alert('everything is working')
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import {pageLoad} from './page_load'
+
+const content = document.getElementById('content');
+content.appendChild(pageLoad());
